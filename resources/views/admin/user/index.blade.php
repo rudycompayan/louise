f36088314a28f0c52bbf4f557cf6fc0eaaedaadd
Rudy Compayan <tatang.greatdev@gmail.com>
2017-11-17 09:13:44 +0800

    Add existing file2

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -51,7 +51,7 @@
                                 <td>{{ $user->age }}</td>
                                 <td>
                                     <input type="hidden" id="link{{ $user->user_id }}" value="{{ $user->fb_profile }}">
-                                    <a href="{{ route('admin.user.edit', $user->user_id) }}"
+                                    <a href="#"
                                        class="btn btn-success btn-xs cc_copy" id="{{ $user->user_id }}" title="Copy Link"><i class="fa fa-copy"></i></a>
                                     <a href="{{ route('admin.user.edit', $user->user_id) }}"
                                        class="btn btn-success btn-xs" title="Edit User"><i class="fa fa-edit"></i></a>
